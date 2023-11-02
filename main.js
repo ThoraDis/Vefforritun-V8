2191a169f93f37234460891db4ca49edb4026392
ThoraDis <142800101+ThoraDis@users.noreply.github.com>
2023-11-02 21:46:35 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,133 @@
+import { createCartLine, showCartContent } from './lib/ui.js';
+
+/**
+ * @typedef {Object} Product
+ * @property {number} id Auðkenni vöru, jákvæð heiltala stærri en 0.
+ * @property {string} title Titill vöru, ekki tómur strengur.
+ * @property {string} description Lýsing á vöru, ekki tómur strengur.
+ * @property {number} price Verð á vöru, jákvæð heiltala stærri en 0.
+ */
+
+const cart = [];
+
+const products = [
+  {
+    id: 1,
+    title: 'HTML húfa',
+    description:
+      'Húfa sem heldur hausnum heitum og hvíslar hugsanlega að þér hvaða element væri best að nota.',
+    price: 5_000,
+  },
+  {
+    id: 2,
+    title: 'CSS sokkar',
+    description: 'Sokkar sem skalast vel með hvaða fótum sem er.',
+    price: 3_000,
+  },
+  {
+    id: 3,
+    title: 'JavaScript jakki',
+    description: 'Mjög töff jakki fyrir öll sem skrifa JavaScript reglulega.',
+    price: 20_000,
+  },
+];
+
+/**
+ * Bæta vöru í körfu
+ * @param  {Product} product
+ * @param {number} quantity 
+ */
+function addProductToCart(product, quantity) {
+  const cartTableBodyElement = document.querySelector('.cart table tbody');
+  cart.push({ product, quantity });
+
+  if (!cartTableBodyElement) {
+    console.warn('fann ekki .cart table');
+    return;
+  }
+  
+  // TODO hér þarf að athuga hvort lína fyrir vöruna sé þegar til
+  const cartLine = createCartLine(product, quantity);
+  cartTableBodyElement.appendChild(cartLine);
+
+
+  // Sýna efni körfu
+  showCartContent(true);
+
+  // TODO sýna/uppfæra samtölu körfu
+ 
+  calculateAndSetCartTotal(cart);
+
+}
+
+function calculateAndSetCartTotal(cart) {
+  let total = 0;
+
+  for (const cartItem of cart) {
+    total += cartItem.product.price * cartItem.quantity;
+  }
+
+  const totalElement = document.querySelector('.table tfoot .price');
+  if (totalElement) {
+    totalElement.textContent = total + ' kr.-';
+  } 
+
+}
+
+function submitHandler(event) {
+  // Komum í veg fyrir að form submiti
+  event.preventDefault();
+  
+  // Finnum næsta element sem er `<tr>`
+  const parent = event.target.closest('tr')
+
+
+  // Það er með attribute sem tiltekur auðkenni vöru, t.d. `data-product-id="1"`
+  const productId = Number.parseInt(parent.dataset.productId);
+
+  // Finnum vöru með þessu productId
+  const product = products.find((i) => i.id === productId);
+
+  // TODO hér þarf að finna fjölda sem á að bæta við körfu með því að athuga
+  // á input
+  const quantityInput = parent.querySelector('input');
+  const quantity = parseInt(quantityInput.value);
+
+  // Bætum vöru í körfu (hér væri gott að bæta við athugun á því að varan sé til)
+
+  addProductToCart(product, quantity);
+
+}
+
+
+// Finna öll form með class="add"
+const addToCartForms = document.querySelectorAll('.add');
+
+// Ítra í gegnum þau sem fylki (`querySelectorAll` skilar NodeList)
+for (const form of Array.from(addToCartForms)) {
+  // Bæta submit event listener við hvert
+  form.addEventListener('submit', submitHandler);
+}
+
+// TODO bæta við event handler á form sem submittar pöntun
+
+document.addEventListener('DOMContentLoaded', function() {
+  const reloadLink = document.getElementById('reloadLink');
+
+  if (reloadLink) {
+    reloadLink.addEventListener('click', function(event) {
+      event.preventDefault(); 
+      location.reload(); 
+    });
+  }
+});
+
+
+  
+  
+
+
+
+
+
+
